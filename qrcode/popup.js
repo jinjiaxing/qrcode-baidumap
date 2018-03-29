40a09440e61f46f5b9995d1e6dafbc4b18f6ca3c
jinjiaxing <jinjiaxing@baidu.com>
2018-03-29 13:57:27 +0800

    update lib & business

--- qrcode/popup.js
+++ qrcode/popup.js
@@ -16,7 +16,8 @@ window.addEventListener("load", function() {
 var initQrcode = function(url) {
 	var pre = {
 		'web': url,
-		'map': 'baidumap://map/cost_share?url=' + url,
+		'map': 'baidumap://map/cost_share?url=' + encodeURIComponent(url)
+		//'baidumap://map/cost_share?url=' + encodeURIComponent(url),
 		// 'lbc': 'baidumap://map/component?comName=lbc&target=webshell_login_page&param=' +
 		// encodeURIComponent(JSON.stringify({'url':url})),
 		// 'nuo': 'bainuo://component?url=' + encodeURIComponent(url),
